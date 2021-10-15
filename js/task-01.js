29deb9e57598fb0aaa8206763548406974272227
Valerii Kurochka <78504019+Va110k@users.noreply.github.com>
2021-10-15 20:06:53 +0300

    done task 1

--- js/task-01.js
+++ js/task-01.js
@@ -3,3 +3,15 @@ function categoriesNumbers() {
   return console.log(`В списке ${getCategories.length} категории`);
 }
 categoriesNumbers();
+
+function categoriesAll() {
+  const getCategories = document.querySelectorAll("li.item");
+  getCategories.forEach((category) => {
+    console.log(
+      `Категория: ${category.querySelector("h2").textContent}
+     Количество элементов: ${category.querySelectorAll("li").length}`
+    );
+  });
+}
+
+categoriesAll();
